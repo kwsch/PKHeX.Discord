b68838637bc09f35d68eb4c001bc6b648856bd9d
Kurt <kaphotics@gmail.com>
2019-12-29 22:38:09 -0800

    Prettify find response with embed
    parse the response

--- PKHeX.Discord/Commands/LearnInfo.cs
+++ PKHeX.Discord/Commands/LearnInfo.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using PKHeX.Core;
 
@@ -27,9 +28,46 @@ namespace PKHeX.Discord
         {
             var args = context.Split(", ");
             var species = args[0];
+            var builder = new EmbedBuilder
+            {
+                Color = new Color(114, 137, 218),
+                Description = $"Here's where you can find {species}:"
+            };
+
             var summary = EncounterLearn.GetLearnSummary(species, args.Skip(1));
-            var msg = string.Join(Environment.NewLine, summary);
-            await ReplyAsync(msg).ConfigureAwait(false);
+            var sb = new StringBuilder();
+            var key = string.Empty;
+            bool any = false;
+            foreach (var line in summary)
+            {
+                if (line.StartsWith("="))
+                {
+                    any = true;
+                    if (sb.Length > 0)
+                    {
+                        var key1 = key;
+                        builder.AddField(x =>
+                        {
+                            x.Name = key1;
+                            x.Value = sb.ToString();
+                            x.IsInline = false;
+                        });
+                    }
+                    key = line.Replace("=", "");
+                    sb.Clear();
+                    continue;
+                }
+
+                sb.AppendLine(line);
+            }
+
+            if (!any)
+            {
+                await ReplyAsync("None").ConfigureAwait(false);
+                return;
+            }
+
+            await ReplyAsync(null, false, builder.Build()).ConfigureAwait(false);
         }
     }
 }
