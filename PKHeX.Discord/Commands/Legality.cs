d5710b724b9d3616a9ab4f0a7661cff48fd88314
Kurt <kaphotics@gmail.com>
2019-12-29 16:54:56 -0800

    Add legality check commands
    lc = legality check
    lcv = ^ with verbose report

---
+++ PKHeX.Discord/Commands/Legality.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using PKHeX.Core;
+
+namespace PKHeX.Discord
+{
+    public class Legality : ModuleBase<SocketCommandContext>
+    {
+        private static readonly WebClient webClient = new WebClient();
+
+        [Command("lc")]
+        [Summary("Verifies the attachment for legality.")]
+        public async Task LegalityCheck()
+        {
+            var attachments = Context.Message.Attachments;
+            foreach (var att in attachments)
+                await LegalityCheck(att, false).ConfigureAwait(false);
+        }
+
+        [Command("lcv")]
+        [Summary("Verifies the attachment for legality with a verbose output.")]
+        public async Task LegalityCheckVerbose()
+        {
+            var attachments = Context.Message.Attachments;
+            foreach (var att in attachments)
+                await LegalityCheck(att, true).ConfigureAwait(false);
+        }
+
+        private async Task LegalityCheck(IAttachment att, bool verbose)
+        {
+            if (att.Size > 0x158)
+            {
+                await ReplyAsync($"{att.Filename}: Invalid size.").ConfigureAwait(false);
+                return;
+            }
+
+            string url = att.Url;
+
+            // Download the resource and load the bytes into a buffer.
+            byte[] buffer = await webClient.DownloadDataTaskAsync(url).ConfigureAwait(false);
+            var pkm = PKMConverter.GetPKMfromBytes(buffer);
+            if (pkm == null)
+            {
+                await ReplyAsync($"{att.Filename}: Invalid pkm attachment.").ConfigureAwait(false);
+                return;
+            }
+
+            var la = new LegalityAnalysis(pkm);
+            await ReplyAsync($"Legality Report for {att.Filename}:").ConfigureAwait(false);
+            await ReplyAsync($"{la.Report(verbose)}").ConfigureAwait(false);
+        }
+    }
+}
