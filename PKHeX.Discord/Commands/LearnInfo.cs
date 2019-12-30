057c332def7ff6b48e9283173cfbcc349e413248
Kurt <kaphotics@gmail.com>
2019-12-29 16:36:38 -0800

    Add Learn/Encounter

--- PKHeX.Discord/Commands/LearnInfo.cs
+++ PKHeX.Discord/Commands/LearnInfo.cs
@@ -9,7 +9,21 @@ namespace PKHeX.Discord
     public class LearnInfo : ModuleBase<SocketCommandContext>
     {
         [Command("learn")]
+        [Summary("Checks if the pkm can learn all of the moves asked.")]
         public async Task LearnAsync([Remainder]string context)
+        {
+            var args = context.Split(", ");
+            var species = args[0];
+            var summary = EncounterLearn.CanLearn(species, args.Skip(1));
+            var msg = summary
+                ? $"Yep! {species} can learn {string.Join(", ", args.Skip(1))}."
+                : $"Nope, {species} can't' learn {string.Join(", ", args.Skip(1))}";
+            await ReplyAsync(msg).ConfigureAwait(false);
+        }
+
+        [Command("encounter")]
+        [Summary("Returns a list of encounter locations where a pkm can be found, to learn all of the moves asked.")]
+        public async Task EncounterAsync([Remainder]string context)
         {
             var args = context.Split(", ");
             var species = args[0];
