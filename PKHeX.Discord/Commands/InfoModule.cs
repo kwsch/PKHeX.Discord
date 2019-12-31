ce13b55981f03b4421ea92e459fb184b7698b116
Kurt <kaphotics@gmail.com>
2019-12-31 14:45:26 -0800

    Add info module

---
+++ PKHeX.Discord/Commands/InfoModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace PKHeX.Discord
+{
+    // src: https://github.com/foxbot/patek/blob/master/src/Patek/Modules/InfoModule.cs
+    // ISC License (ISC)
+    // Copyright 2017, Christopher F. <foxbot@protonmail.com>
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        private const string detail = "I am an open source Discord bot powered by PKHeX.Core and other open source software.";
+        private const string repo = "https://github.com/kwsch/PKHeX.Discord";
+
+        [Command("info")]
+        [Alias("about", "whoami", "owner")]
+        public async Task InfoAsync()
+        {
+            var app = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+
+            await ReplyAsync($"{detail}\n" +
+                "\n" +
+                $"{Format.Bold("Info")}\n" +
+                $"- Source Code: {Format.EscapeUrl(repo)}\n" +
+                $"- Author: {app.Owner} ({app.Owner.Id})\n" +
+                $"- Library: Discord.Net ({DiscordConfig.Version})\n" +
+                $"- Runtime: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.ProcessArchitecture} " +
+                $"({RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture})\n" +
+                $"- Uptime: {GetUptime()}\n" +
+                "\n" +
+                $"{Format.Bold("Stats")}\n" +
+                $"- Heap Size: {GetHeapSize()}MiB\n" +
+                $"- Guilds: {Context.Client.Guilds.Count}\n" +
+                $"- Channels: {Context.Client.Guilds.Sum(g => g.Channels.Count)}\n" +
+                $"- Users: {Context.Client.Guilds.Sum(g => g.Users.Count)}\n"
+                ).ConfigureAwait(false);
+        }
+
+        private static string GetUptime() => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
+        private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString();
+    }
+}
