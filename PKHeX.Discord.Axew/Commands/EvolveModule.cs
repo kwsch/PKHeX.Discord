6a95ad2adc882b2b6184b05d2ec721d483d993cc
Kurt <kaphotics@gmail.com>
2020-01-20 21:59:43 -0800

    Push latest run content
    update automod dll from a few days ago

---
+++ PKHeX.Discord.Axew/Commands/EvolveModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace PKHeX.Discord.Axew
+{
+    public class EvolveModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("evolve"), Alias("evolve")]
+        [Summary("Prints Evolution data for the species.")]
+        public async Task PrintDataAsync([Summary("Generation")]int gen, [Summary("Species Index")]int species)
+        {
+            await ReplyAsync("Sorry, I don't handle this command yet!").ConfigureAwait(false);
+        }
+    }
+}
